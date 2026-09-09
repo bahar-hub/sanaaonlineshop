1671416a0d9a23d96e9151e6014f029b2cfcad01
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-09-09 21:23:26 +0330

    Forgot password with otp
    start building forgot passwprd

--- customer/models.py
+++ customer/models.py
@@ -14,3 +14,25 @@ class User(AbstractUser):
         null=False,
         blank=False,
     )
+
+
+class PasswordResetOTP(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="password_reset_otps",
+    )
+
+    code_hash = models.CharField(max_length=64)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    expires_at = models.DateTimeField()
+
+    attempts = models.PositiveIntegerField(default=0)
+
+    is_used = models.BooleanField(default=False)
+
+    def is_expired(self):
+        from django.utils import timezone
+        return timezone.now() >= self.expires_at
--- sana/settings.py
+++ sana/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,5 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+MELIPAYAMAK_OTP_URL = config("MELIPAYAMAK_OTP_URL")
 AUTH_USER_MODEL = "customer.User"
