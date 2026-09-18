6572354c73869f85214ba1d1333eb9e0917a35d8
amirmhmd.hoseini <amirmhmd.hosini@gmail.com>
2026-09-18 12:14:51 +0330

    Update settings.py

--- sana/settings.py
+++ sana/settings.py
@@ -21,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eqwpd-34z!4s(5qkp=@2cv$+5)y^7owkw%y_n4*4&j(6m52xjr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS', default='').split(',')
+    if host.strip()
+]
 
 # Application definition
 
